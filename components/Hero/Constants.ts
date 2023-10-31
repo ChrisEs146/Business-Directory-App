71e77a772672bf7dbd0e64f6ff2e2626d575db8b
ChrisEs146 <chris.es146@gmail.com>
2023-10-31 16:26:01 -0600

    Add: add constants for the hero section

---
+++ components/Hero/Constants.ts
@@ -0,0 +1,38 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Restaurant",
+    img: "/restaurant.png",
+    description: "A restaurant icon",
+  },
+  {
+    id: 2,
+    name: "Bar",
+    img: "/bar.png",
+    description: "Illustration of a drink",
+  },
+  {
+    id: 3,
+    name: "Bank",
+    img: "/bank.png",
+    description: "Illustration of a bank",
+  },
+  {
+    id: 4,
+    name: "Gas station",
+    img: "/gas.png",
+    description: "Illustration of a gas station",
+  },
+  {
+    id: 5,
+    name: "Supermarket",
+    img: "/market.png",
+    description: "Illustration of a supermarket cart",
+  },
+  {
+    id: 6,
+    name: "Coffee shop",
+    img: "/coffee.png",
+    description: "Illustration of a coffee cup",
+  },
+];
