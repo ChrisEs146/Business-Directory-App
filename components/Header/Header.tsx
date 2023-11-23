8a322284d83741d0df9283182afef1f774b8e9ea
ChrisEs146 <chris.es146@gmail.com>
2023-11-23 17:29:56 -0600

    Update: update header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,32 +1,30 @@
 import Image from "next/image";
-import { linkItems } from "./Constants";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-4 shadow-md">
-      <div className="flex gap-2 items-center">
-        <Image
-          src="/logo.png"
-          alt="Discover, business directory logo"
-          width={50}
-          height={50}
-        />
-        <h2 className="text-xl text-red-500 tracking-widest font-semibold">
-          DISCOVER
-        </h2>
+    <header className="p-2 shadow-md">
+      <div className="flex justify-between items-center max-w-7xl my-0 mx-auto">
+        <Link className="flex gap-2 items-center" href="/" title="Homepage">
+          <Image
+            src="/logo.png"
+            alt="Discover, business directory logo"
+            width={40}
+            height={40}
+          />
+          <h2 className="text-xl text-red-500 tracking-widest font-semibold">
+            DISCOVER
+          </h2>
+        </Link>
+        <nav>
+          <Link
+            href="/about"
+            className="text-lg hover:text-red-500 p-2 border-2 border-red-500 rounded-full"
+          >
+            About
+          </Link>
+        </nav>
       </div>
-      <nav>
-        <ul className="flex gap-5 items-center">
-          {linkItems.map((item) => (
-            <li
-              className="text-lg hover:text-red-500 cursor-pointer"
-              key={item.id}
-            >
-              {item.label}
-            </li>
-          ))}
-        </ul>
-      </nav>
     </header>
   );
 }
