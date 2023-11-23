d42e5c9627a5099f3d596ed104e13de517e7a51d
ChrisEs146 <chris.es146@gmail.com>
2023-11-23 17:31:15 -0600

    Add: add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section className="max-w-7xl my-0 mx-auto">
+      <h1 className="text-3xl font-bold my-3">About</h1>
+      <p className="mb-3">
+        This is a demo project to showcase how to build a business directory
+        using Next.js, TailwindCSS, and Google Maps API.
+      </p>
+      <p className="mb-3">The project is open source and available on </p>
+    </section>
+  );
+}
