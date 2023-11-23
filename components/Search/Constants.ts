3144a22e4ea1835e309ce69daedcde9bbf923623
ChrisEs146 <chris.es146@gmail.com>
2023-11-23 17:29:22 -0600

    Add: add constants

---
+++ components/Search/Constants.ts
@@ -0,0 +1,44 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Restaurant",
+    img: "/restaurant.png",
+    description: "A restaurant icon",
+    value: "restaurants",
+  },
+  {
+    id: 2,
+    name: "Bar",
+    img: "/bar.png",
+    description: "Illustration of a drink",
+    value: "bars",
+  },
+  {
+    id: 3,
+    name: "Bank",
+    img: "/bank.png",
+    description: "Illustration of a bank",
+    value: "banks",
+  },
+  {
+    id: 4,
+    name: "Gas station",
+    img: "/gas.png",
+    description: "Illustration of a gas station",
+    value: "gas_stations",
+  },
+  {
+    id: 5,
+    name: "Supermarket",
+    img: "/market.png",
+    description: "Illustration of a supermarket cart",
+    value: "supermarkets",
+  },
+  {
+    id: 6,
+    name: "Coffee shop",
+    img: "/coffee.png",
+    description: "Illustration of a coffee cup",
+    value: "coffee_shops",
+  },
+];
