bb9f056c58d3cffaa823b895f0214761578f8dca
ChrisEs146 <chris.es146@gmail.com>
2023-12-06 17:43:35 -0600

    Update: update main page
    Hero component was deleted. The logic was moved to the main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Hero from "@/components/Hero/Hero";
+import Image from "next/image";
+import CategoryList from "@/components/Search/CategoryList";
+import Search from "@/components/Search/Search";
 
 export default function Home() {
   return (
-    <main className="pb-4">
-      <Hero />
-    </main>
+    <section>
+      <Image
+        className="w-full absolute h-full opacity-30"
+        src="/bg.png"
+        alt="Illustration of a city at night"
+        width={900}
+        height={500}
+      />
+      <div className="text-center pt-10 isolate mb-8">
+        <h1 className="text-4xl tracking-widest font-semibold text-red-500 mb-3">
+          DISCOVER
+        </h1>
+        <h2 className="text-lg text-gray-700 mb-20">Your amazing city</h2>
+      </div>
+      <Search />
+      <CategoryList />
+    </section>
   );
 }
